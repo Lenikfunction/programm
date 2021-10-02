4d8110cabcdfffcfd769a7666c4e9ca24b5d9448
Lenikfunction <icegameleo@yandex.ru>
2021-10-02 13:22:02 +0300

    работа со словарем подсчитывает сколько одинаковых слов в строке введенным пользователем

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
+catalog = {
 
-
-heelo =123
-test = 123asd
-
-hi =234
-
-
-123 =get
+}
+user = input('введите строку__').replace(',','').replace('.','').lower().split()
+for el in user:
+    if not el in catalog:
+        catalog[el] = 1
+    else:
+        catalog[el] += 1
+for k in catalog:
+    print(k,':',catalog[k])
